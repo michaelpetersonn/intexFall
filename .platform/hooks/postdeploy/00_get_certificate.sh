d3ebd29aee244ef9da3c83fcbc1561c1ba108392
Mhawk101 <166653229+Mhawk101@users.noreply.github.com>
2025-12-03 15:36:42 -0700

    adding https to both domains

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 # .platform/hooks/postdeploy/00_get_certificate.sh
-sudo certbot -n -d ellarises2-15.is404.net --nginx --agree-tos --email mhawkin9@byu.edu
+
+sudo certbot -n \
+  --nginx \
+  --agree-tos \
+  --email mhawkin9@byu.edu \
+  -d ellarises2-15.is404.net \
+  -d intexfall2-15.us-east-2.elasticbeanstalk.com
